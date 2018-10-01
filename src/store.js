7c0af1f385da05ab6ef961da562db363e22b7772
Alla Yakubova <yakubova92@gmail.com>
2018-10-01 16:21:04 -0400

    accessing info (title & list of links) in state and rendering it

--- src/store.js
+++ src/store.js
@@ -5,7 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'my custom title',
+    links: [
+      'http://google.com',
+      'http://facebook.com',
+      'http://youtube.com'
+    ]
   },
   mutations: {
 
