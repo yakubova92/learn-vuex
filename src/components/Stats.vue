5f1911569d867d393788838dc8730e6e4e00d170
Alla Yakubova <yakubova92@gmail.com>
2018-10-01 17:46:05 -0400

    split into two components, added a getter to count num of links, added styling

---
+++ src/components/Stats.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="stats">
+    <h1> summary </h1>
+    <p> There are currently {{ countLinks }} links. </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Stats',
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
